2ce2f518072307a08cdb574ad56b3d0e79f8e783
Ubuntu <cs2830@xiaobentu.z5fwejobuapurkdvii1tuesvua.gx.internal.cloudapp.net>
2016-03-21 04:25:59 +0000

    new a7 files

---
+++ a7/a7_index.php
@@ -0,0 +1,42 @@
+<!DOCTYLE html>
+<html>
+
+    <head>
+    <title> A7</title>
+     </head>
+       <style>
+
+            h1, #anphotos {
+
+                text-align: center;
+                font-family: arial;
+                color: #333;
+
+            }
+
+            #anphotos > img {
+                height: 100px;
+            }
+
+        </style>
+    <body>
+
+    <h1>Animal Photos</h1>
+            
+		<div id="anphotos">
+            
+        <?php
+        $imgs = glob("*.jpg");//put all the images to the imgs array
+        foreach($imgs as $img){//pull each images 
+        echo'<img src="'.$img.'" />';//'.variable.'
+        }
+        ?>
+            
+        </div>
+        </body>
+
+    
+    
+</html>
+
+
