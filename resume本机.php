cd207dbb01632c2349d31ab0d20470ee502b6735
zhongyu0221 <zhongyu0221@gmail.com>
2016-04-10 14:45:44 -0500

    finalproject

---
+++ resume本机.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+
+    
+   <!-- <style>
+        .bar{
+            overflow: auto;
+            background-color: black;
+        }
+        .bar li{
+            float:left;
+        }
+        
+        .bar li a{
+            display: block;
+            color:white;
+            text-align: center;
+            padding:14px 16px;
+            text-decoration: none;
+        }
+        #title{
+            font-size:20px;
+            align-content: center;
+            color:aquamarine;
+        }
+        
+        .base{ 
+             color:bisque;
+        }
+        
+        #summary{
+            
+            background: coral;
+            padding: 10px;
+            margin: 5px;
+            width:200px;
+            float:left;
+            
+        }
+    </style>-->
+    
+    
+    <head>
+        <meta charset="UTF-8">
+          <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet" >
+        
+
+    <title> Zhongyu Li' Resume</title>
+    </head>
+    
+<body>
+    
+   <!--for text  <button class="btn btn-info">Bootstrap</button>-->
+
+    <h1 id="title">Zhongyu Li' Resume</h1>
+    
+    <!--header section-->
+    <ul class="nav nav-pills">
+        <li class="active"><a href="#aboutme" >About Me</a></li>
+        <li><a href="#timeline" >Time Line</a></li>
+        <li><a href="#skills" >Skills</a></li>
+        <li><a href="#contactme" >Contact Me</a></li>
+    </ul>
+    
+    
+      <!--header section-->
+    
+    
+    
+    <!--aboutme section-->
+        <div class="aboutme">
+            <div class=".container"><!--定义一个容器-->
+                <div class = "row">
+                <div class="col-md-3"> 
+                    <h1>Zhongyu Li</h1>
+                    <div class="media">
+                    <img class="media-object" src="IMG_4203.JPG">                    
+                    </div>
+                    <h3><a href="Resume zhongyu li.docx" >My resume</a></h3>
+                    
+                    </div>
+                 <div class="col-md-6" id="summary"> 
+    <p>My name is Zhongyu Li. I am a senior student in bioengineering. After finished two years’ study in China, I transferred to Mizzou as a junior student. I took 52 credit hours in MIZZOU so far and got cumulative GPA 3.89. As an engineering student, I believe computer science is an important tool to all engineers. In order to improve my ability in computer area, I took fundamental classes and study a minor degree. Because of the academic achievements, I got a position in Dean's high honor roll for the past 4 semester.</p>
+                     
+ 	<p>I work as a research assistant with Dr. Sengupta for almost two years. My first project is about the rapid culture-based detection of mycobacteria using multi-frequency impedance measurements, which can help reducing the time-to-detection of tuberculosis and make benefits to human. Worked with two graduates and two undergraduates, I getting lots of help from them. In 2015 Spring Undergraduate Research and Creative Achievements Forum, I got the Chancellor's Awards for Excellence in Undergraduate Research in the Engineering Sciences category. Now I am working in an individual project called "Modeling the Effect of Bacterial Load on Minimum Inhibitory Concentration(MIC)". This model can be used in prediction the bacteria's concentration after over times, which can be used both in laboratory of clinical application. 
+    I also work as a tutor in A Way With Works&Number since this semester. A3W&N is a community-based non-profit program that teams undergraduate students from the University of Missouri (MU) with local children to improve their struggling literacy and mathematics skills. I spend more than 5 hours per week to help children in high school. This volunteer experience makes me full and better involved in American society.</p>
+                     </div>
+                    </div>
+                
+                
+                
+                
+    <div class="header">
+                    <div class="avatar">
+                        <a href="javascript:;" class="avatar_pic circle"><em class="circle doing">Details</em></a>
+                        <b class="doing r5">Female</b>
+                    </div>
+        
+                    <div class="base">
+                        <p class="desc">Hello my name is</p>
+                        <h2>Zhongyu Li</h2>
+                        <p>Bioengneering/Computer Science</p>
+                        <span>
+                            <p><i aria-hidden="true" data-icon="&#xe01d;"></i>Univerisity of missouri-Columbia</p>
+                            <p><a href="javascript:;" class="tel_me"><i aria-hidden="true" data-icon="&#xe00b;"></i>Contact Me</a></p>
+                        </span>
+                    </div>
+                </div>
+    
+   
+        </div>
+    </div>
+       <!--aboutme section-->
+    
+    
+    
+                      <!--timeline section-->
+            <div class="timeline">
+                        <h5><i aria-hidden="true" data-icon="&#x7d;"></i>Timeline</h5>
+                        <ul>
+                            
+                            <li class="clearfix top" id="s5">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Dr.Sengupta's microfluidic lab</h3>
+                                    <p>Research Assistance</p>
+                                    <p class="desc"> October 2014-current</p>
+                                </div>
+                            </li>
+                            
+                            <li class="clearfix" id="s4">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>A Way With Words And Numbers</h3>
+                                    <p> Tutor</p>
+                                    <p class="desc"> February 2016-current</p>
+                                </div>
+                            </li>
+                        
+                            <li class="clearfix" id="s3">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>Jiangnan University</h3>
+                                    <p> Bachelor of Biological Engineering</p>
+                                    <p class="desc"> August 2012-May 2014</p>
+                                </div>
+                            </li>
+                            <li class="clearfix" id="s2">
+                                <!--list样式-->
+                                <div class="dot"><b></b></div>
+                                <!--内容-->
+                                <div class="con">
+                                    <h3>University of Missouri-Columbia</h3>
+                                    <p> Major: Bachelor of Bioengineering(BioMed)	 
+                                        Minor: Computer Science</p>
+                                    <p class="desc">2014.1 - 2015.1</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+         <!--timeline section-->
+    
+     <!--skills section-->
+        
+        <div class= "skiils">
+        
+        </div>
+        
+        <!--skills section-->
+    
+        
+    <!--contactme section-->
+        <div class="contactme">
+        
+        </div>
+    
+      <!--contactme section-->
+    
+   
+</body>
+</html>
+    
+
+
+
